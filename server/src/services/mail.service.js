3c1f521af97ce75ef1a8446750185ede531f3785
Ashish <khatriaashish1@gmail.com>
2024-05-18 23:46:03 +0545

    Minor fixes

--- server/src/services/mail.service.js
+++ server/src/services/mail.service.js
@@ -6,8 +6,10 @@ class MailService {
   constructor() {
     try {
       this.transport = nodemailer.createTransport({
+        service: process.env.SMTP_SERVICE,
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
+        secure: true,
         auth: {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASS,
